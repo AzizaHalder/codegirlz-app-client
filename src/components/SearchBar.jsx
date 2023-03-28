ae7da67dd212d325988bb187c757e4505af8f8c2
AzizaHalder <aziza.halder@sap.com>
2023-03-28 16:02:28 +0200

    feat(searchBar): WIP clear button search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,38 @@
+// import { useState } from "react";
+
 const SearchBar = ({ onQuery }) => {
+  //   const [query, setQuery] = useState("");
+
   return (
     <div className="SearchBar">
       <label htmlFor="">Search </label>
-      <input type="text" onChange={(e) => onQuery(e.target.value)} />
+      <input
+        type="text"
+        // value={query}
+        onChange={(e) => {
+          onQuery(e.target.value);
+        }}
+      />
+
+      {/**@todo implement clear search button */}
+
+      {/* <input
+        type="text"
+        value={query}
+        onChange={(e) => {
+          setQuery(e.target.value);
+          onQuery(query);
+        }}
+      /> */}
+
+      {/* <button
+        onClick={(e) => {
+          setQuery("");
+          onQuery(query);
+        }}
+      >
+        ❌
+      </button> */}
     </div>
   );
 };
