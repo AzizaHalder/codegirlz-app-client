e0de332c0574a104ecd4d24ccc4436caeaab82d1
Nicole <92593875+ozzleme@users.noreply.github.com>
2023-04-02 00:03:35 +0530

    feat(navbar): bootstrap and dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,101 +3,148 @@ import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from "@fortawesome/free-solid-svg-icons";
+import { faUsers } from "@fortawesome/free-solid-svg-icons";
 import logo from "../images/logo.png";
 import Button from "react-bootstrap/Button";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
+import { faLaptopFile } from '@fortawesome/free-solid-svg-icons'
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons'
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { useState } from 'react';
 
 function NavBar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Nav className="justify-content-center" fixed="top" id="nav-bar">
-          <Navbar.Brand>
-            <Link to="/">
-              <Button justify variant="link">
-                <img src={logo} alt="logo" id="logo-button"></img>
-              </Button>
-            </Link>
-          </Navbar.Brand>
 
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav.Item>
-              <span>Welcome {user && user.name} 👋 </span>
-            </Nav.Item>
-            <Nav.Item>
-              <Link to="/meetup">
-                <Button justify variant="light">
-                  Meetups
-                </Button>
-              </Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link to="/resource">
-                <Button justify variant="light">
-                  Resources
-                </Button>
-              </Link>
-            </Nav.Item>
+    <div id="nav-container" className="justify-content-center">
 
-            {isLoggedIn && (
-              <>
-                <Nav.Item>
-                  <Link to="/meetup/create">
-                    <Button justify variant="light">
-                      Create Meetup
-                    </Button>
+      {isLoggedIn && (
+        <>
+          <Navbar collapseOnSelect expand="sm" className="justify-content-center" >
+            <Container fluid>
+              <div id="nav-container-logged-in">
+                <Navbar.Brand>
+                  <Link to="/" className="navbar-brand">
+                    <img src={logo} alt="logo" id="logo-button"></img>
                   </Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Link to="/auth/attend">
-                    <button>Attend Meetup</button>
-                  </Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Link to="/resource/create">
-                    <Button justify variant="light">
-                      Add Resource
-                    </Button>
-                  </Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Link to="/auth/save">
-                    <Button justify variant="light">
-                      My Saved Items{" "}
-                      <FontAwesomeIcon
-                        icon={faBookmark}
-                        size="lg"
-                        style={{ color: "#32612d" }}
-                      />
-                    </Button>
-                  </Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Button justify variant="light" onClick={logOutUser}>
-                    Logout
-                  </Button>
-                </Nav.Item>
-              </>
-            )}
+                </Navbar.Brand>
+                <Nav className="justify-content-center" navbarScroll fixed="top" id="nav-bar" >
+                  <div id="nav-drop-down-menu">
+                    <Navbar.Toggle aria-controls="navbarScroll" id="nav-toggle" />
+                    <Navbar.Collapse id="navbarScroll" >
+                      <ul>
+                        <li>
+                          <NavDropdown title={<FontAwesomeIcon icon={faUsers} style={{ color: "#81B4A6", }} />} id="navbarScrollingDropdown" className="dropdown-menu-end" >
+                            <NavDropdown.Item><Link to="/meetup">
+                              Browse Meetups
+                            </Link>
+                            </NavDropdown.Item>
+                            <NavDropdown.Item >
+                              <Link to="/meetup/create">
+                                Create Meetup
+                              </Link>
+                            </NavDropdown.Item>
+                            <NavDropdown.Item >
+                              <Link to="/auth/attend">
+                                Attend Meetup
+                              </Link>
+                            </NavDropdown.Item>
+                          </NavDropdown>
+                        </li>
+                        <li>
+                          <NavDropdown title={<FontAwesomeIcon icon={faLaptopFile} style={{ color: "#81B4A6", }} />} id="collasible-nav-dropdown">
+                            <NavDropdown.Item><Link to="/resource">
+                              Browse Resources
+                            </Link>
+                            </NavDropdown.Item>
+                            <NavDropdown.Item >
+                              <Link to="/resource/create">
+                                Add Resource
+                              </Link>
+                            </NavDropdown.Item>
+                          </NavDropdown>
 
-            {!isLoggedIn && (
-              <>
-                <Nav.Item>
-                  <Link to="/auth/login">
-                    <Button variant="light">Login</Button>
-                  </Link>
-                </Nav.Item>
-              </>
-            )}
-          </Navbar.Collapse>
-        </Nav>
-      </Container>
-    </Navbar>
+                        </li>
+
+                        <li>
+                          <Nav.Item>
+                            <Link to="/auth/save">
+                              <FontAwesomeIcon
+                                icon={faBookmark}
+                                style={{ color: "#81B4A6" }}
+                              />
+                            </Link>
+                          </Nav.Item>
+                        </li>
+                        <li>
+                          <Nav.Item onClick={logOutUser}>
+                            Logout
+                          </Nav.Item>
+                        </li>
+                      </ul>
+                    </Navbar.Collapse>
+                  </div>
+                </Nav>
+              </div>
+            </Container>
+          </Navbar>
+        </>
+
+      )
+      }
+
+      {!isLoggedIn && (
+
+        <><Navbar collapseOnSelect expand="sm" className="justify-content-center" >
+          <Container fluid>
+            <div id="nav-container-logged-in">
+              <Navbar.Brand>
+                <Link to="/" className="navbar-brand">
+                  <img src={logo} alt="logo" id="logo-button"></img>
+                </Link>
+              </Navbar.Brand>
+              <Nav className="justify-content-center" navbarScroll fixed="top" id="nav-bar" >
+                <div id="nav-drop-down-menu">
+                  <Navbar.Toggle aria-controls="navbarScroll" id="nav-toggle" />
+                  <Navbar.Collapse id="navbarScroll" >
+                    <ul>
+                      <li>
+                        <Nav.Item>
+                          <Link to="/meetup">
+                            Meetups {" "}<FontAwesomeIcon icon={faUsers} style={{ color: "#81B4A6", }} />
+                          </Link>
+                        </Nav.Item>
+                      </li>
+                      <li>
+                        <Nav.Item>
+                          <Link to="/resource">
+                            Resources {" "}<FontAwesomeIcon icon={faLaptopFile} style={{ color: "#81B4A6", }} />
+                          </Link>
+                        </Nav.Item>
+                      </li>
+                      <li>
+                        <Nav.Item>
+                          <Link to="/auth/login">
+                            Login{" "}<FontAwesomeIcon icon={faRightToBracket} style={{ color: "#81B4A6", }} />
+                          </Link>
+                        </Nav.Item>
+                      </li>
+                    </ul>
+                  </Navbar.Collapse>
+                </div>
+              </Nav>
+            </div>
+          </Container>
+        </Navbar>
+
+        </>
+      )
+      }
+
+    </div>
   );
 }
 
