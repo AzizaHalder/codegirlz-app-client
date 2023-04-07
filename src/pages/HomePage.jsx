11a216a47df634866e091b7dd63d4ef501eec43b
Nicole <92593875+ozzleme@users.noreply.github.com>
2023-04-07 12:50:36 +0530

    feat(homepage): gallery and list of footer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,6 @@
+import Card from 'react-bootstrap/Card';
+import { Link } from "react-router-dom";
+import ListGroup from 'react-bootstrap/ListGroup';
 
 
 function HomePage() {
@@ -27,8 +30,65 @@ function HomePage() {
                 </div>
                 <img src="https://res.cloudinary.com/ddieot9rc/image/upload/v1680810179/code-girlz/resources_vmpwhx.jpg" alt="meetupImg2" id="image-home" />
             </div>
-            <div>
-
+            <div id="home-gallery">
+                <Card style={{ width: '20rem' }} id="card">
+                    <Link to="/resource" className="navbar-brand">
+                        <Card.Img id="card-image" variant="top" src="https://res.cloudinary.com/ddieot9rc/image/upload/v1680850360/code-girlz/meetupthumbnail1_es8cfm.jpg" />
+                    </Link>
+                </Card>
+                <Card style={{ width: '20rem' }} id="card">
+                    <Link to="/resource" className="navbar-brand">
+                        <Card.Img id="card-image" variant="top" src="https://res.cloudinary.com/ddieot9rc/image/upload/v1680850360/code-girlz/meetupthumbnail3_cujvct.jpg" />
+                    </Link>
+                </Card>
+                <Card style={{ width: '20rem' }} id="card">
+                    <Link to="/resource" className="navbar-brand">
+                        <Card.Img id="card-image" variant="top" src="https://res.cloudinary.com/ddieot9rc/image/upload/v1680850360/code-girlz/meetupthumbnail2_fhkgyt.jpg" />
+                    </Link>
+                </Card>
+                <Card style={{ width: '20rem' }} id="card">
+                    <Link to="/resource" className="navbar-brand">
+                        <Card.Img id="card-image" variant="top" src="https://res.cloudinary.com/ddieot9rc/image/upload/v1680808122/code-girlz/meetup2_g7vfyo.jpg" />
+                    </Link>
+                </Card>
+                <Card style={{ width: '20rem' }} id="card">
+                    <Link to="/resource" className="navbar-brand">
+                        <Card.Img id="card-image" variant="top" src="https://res.cloudinary.com/ddieot9rc/image/upload/v1680850361/code-girlz/meetupthumbnail9_bvcsfu.jpg" />
+                    </Link>
+                </Card>
+                <Card style={{ width: '20rem' }} id="card">
+                    <Link to="/resource" className="navbar-brand">
+                        <Card.Img id="card-image" variant="top" src="https://res.cloudinary.com/ddieot9rc/image/upload/v1680850362/code-girlz/meetupthumbnail8_fnkjqx.jpg" />
+                    </Link>
+                </Card>
+                <Card style={{ width: '20rem' }} id="card">
+                    <Link to="/resource" className="navbar-brand">
+                        <Card.Img id="card-image" variant="top" src="https://res.cloudinary.com/ddieot9rc/image/upload/v1680850362/code-girlz/meetupthumbnail5_lifpil.jpg" />
+                    </Link>
+                </Card>
+                <Card style={{ width: '20rem' }} id="card">
+                    <Link to="/resource" className="navbar-brand">
+                        <Card.Img id="card-image" variant="top" src="https://res.cloudinary.com/ddieot9rc/image/upload/v1680850365/code-girlz/meetupthumbnail6_m5kecv.jpg" />
+                    </Link>
+                </Card>
+            </div>
+            <div id="footer">
+                <Card id="footer-card">
+                    <Card.Header>GirlCoderz</Card.Header>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>Sign Up</ListGroup.Item>
+                        <ListGroup.Item>Meetup</ListGroup.Item>
+                        <ListGroup.Item>Learn</ListGroup.Item>
+                    </ListGroup>
+                </Card>
+                <Card id="footer-card">
+                    <Card.Header>Recruiters</Card.Header>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>Register</ListGroup.Item>
+                        <ListGroup.Item>Hire GirlCoderz</ListGroup.Item>
+                        <ListGroup.Item>GirlCoderz Circles</ListGroup.Item>
+                    </ListGroup>
+                </Card>
             </div>
         </div>
     )
